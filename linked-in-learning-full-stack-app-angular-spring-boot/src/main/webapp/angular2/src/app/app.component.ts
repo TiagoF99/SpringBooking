e54bb8a05c64218d1462a44f6c39d54797147881
tiago <t.ferreira@live.ca>
2019-06-20 11:19:06 -0400

    added some room services and also edited the html and css to accomadate these changes

--- linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
@@ -19,11 +19,15 @@ export class AppComponent implements OnInit {
   public submitted: boolean;
   roomsearch: FormGroup;
 
+
+  rooms:Room[];
+
   ngOnInit() {
   	this.roomsearch = new FormGroup({
   		checkin: new FormControl(""),
   		checkout: new FormControl("")
   	});
+  	this.rooms = ROOMS;
   }
 
   onSubmit({value, valid}: {value: Roomsearch, valid:boolean}) {
@@ -35,3 +39,30 @@ export interface Roomsearch {
 	checkin:String;
 	checkout:String;
 }
+
+/**
+*below here is what is needed to make a room list
+*also in code do as an example to access it:
+* 	<div *ngFor="let room of rooms">
+*  		<div>{{room.price}}</div>
+*	</div>
+*/
+export interface Room {
+	id:string;
+	roomNumber:string;
+	price:string;
+	links:string;
+}
+var ROOMS:Room[] = [
+	{
+		"id":"1",
+		"roomNumber":"411",
+		"price":"28",
+		"links": ""
+	},{
+		"id":"2",
+		"roomNumber":"412",
+		"price":"30",
+		"links": ""
+	}
+]
